07abf580d1bd814b6f42cdfb9c9bc34f029642fe
Lautaro Ocampo <lautaroocampo00@gmail.com>
2022-03-26 22:25:35 -0300

    fixed launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+fuser 3001/tcp -k &
+
+fuser 3000/tcp -k &
+
 # Run the backened server
-java -jar ./target/demo-0.0.1-SNAPSHOT.jar & 
+java -jar ./target/demo-0.0.1-SNAPSHOT.jar &
+
 
 # Run the react frontend in the background
 npm start --prefix ./react-frontend &
